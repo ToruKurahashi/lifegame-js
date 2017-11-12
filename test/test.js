dffbc686f5dfe9994bce23aa6a9bea47283ba52c
mgg-k <lbst010@gmail.com>
2017-11-12 14:48:44 +0900

    modify:[初期化] キャンバスを初期化するテストを追加

--- test/test.js
+++ test/test.js
@@ -7,11 +7,22 @@ describe('ライフゲーム',() => {
 
   test('既存の出力と比較する', () => {
     const lines = [];
-    lifegame((line) => {
+    lifegame.lifegame((line) => {
       lines.push(line + '\n');
     });
     const original = fs.readFileSync(path.join(__dirname, '..', 'original_output'), 'utf-8');
     assert(lines.join('') === original);
+  });
+
+  test('正しく初期化されているかどうか', () => {
+    const data = [
+      ['□','□','□','□','□'],
+      ['□','□','□','□','□'],
+      ['□','□','□','□','□'],
+      ['□','□','□','□','□'],
+      ['□','□','□','□','□']
+    ];
+    assert.deepEqual(lifegame.initCanvas(), data);
   });
 
 });
